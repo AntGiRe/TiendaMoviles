2f96ce114c557d6272ecbb199fe19c7070082338
mariannsss <96204596+mariannsss@users.noreply.github.com>
2022-12-22 12:14:48 +0100

    2º formulario

--- insertar.php
+++ insertar.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario para reparación</title>
+    <title>Insertar reparacion</title>
 </head>
 
 <body>
@@ -29,10 +29,6 @@
             <div>
                 <label for="email">Email:</label>
                 <input type="email" name="email" id="email">
-            </div>
-            <div>
-                <label for="name">Tecnico:</label>
-                <input type="text" name="tecnico" id="tecnico">
             </div>
             <div>
                 <label for="direccion">Problema del movil:</label>
