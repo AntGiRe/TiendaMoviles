f4df0500e9d374879fd0121ad6653604988d4616
Amalia Torre <amaliatorrevillahoz@gmail.com>
2022-12-22 10:46:39 +0100

    form comercial

---
+++ insertar.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <header>
+
+
+  </header>
+
+  <nav>
+
+
+  </nav>
+
+  <main>
+    <!--formulario Comercial -->
+
+    <label for="name">Nombre</label>
+    <input type="text" name="name" id="name">
+    <label for="email">Email</label>
+    <input type="email" name="email" id="email">
+    <label for="direccion">Dirección</label>
+    <textarea id="direccion" name="direccion"></textarea>
+
+
+
+
+  </main>
+
+  <footer>
+
+
+  </footer>
+
+
+
+
+
+</body>
+</html>
